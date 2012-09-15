ff96ffe021ad60ccd735ffa17e570496aaabf824
sreynaga <sergio.reynaga@gmail.com>
2012-09-15 11:21:36 -0700

    Modificacion al archivo de conexion

--- conn.php
+++ conn.php
@@ -1,6 +1,10 @@
 <?php
 
-	mysql_connect("localhost","root","hsrybr08") or die(mysql_error());
-	mysql_select_db("geolocation") or die(mysql_error());
+    $hostname = "nombre_de_tu_hostname"; //Nombre del Hostname de tu base de datos MySQL
+    $user = "nombre_de_usuario"; //Nombre de usuario para conectar a tu base de datos
+    $pass = "password"; //Password para conectar a tu base de datos
+    $db = "nombre_de_tu_base_de_datos"; //Nombre de la base de datos a utilizar
+	mysql_connect($hostname,$user,$pass) or die(mysql_error());
+	mysql_select_db($db) or die(mysql_error());
 
 ?>
